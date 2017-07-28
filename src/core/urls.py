07d42ad138d50713354001b96f9f9fa20906f723
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2017-07-28 18:56:20 -0300

    Added views
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
-from django.views.generic import TemplateView
+from core import views
 
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
+    url(r'^$', views.HomeView.as_view(), name='home'),
+    url(r'^pauta/(?P<pk>\d+)/?$', views.AgendaView.as_view(),
+        name='agenda'),
+    url(r'^pauta/(?P<pk>\d+)/submit/?$', views.SubmitVotesView.as_view(),
+        name='submit_votes'),
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,77 @@
-from django.shortcuts import render
+from django.http import Http404, JsonResponse
+from django.views.generic import ListView, DetailView
+from core import models
+import json
 
-# Create your views here.
+
+class HomeView(ListView):
+    template_name = 'home.html'
+    model = models.Agenda
+    queryset = models.Agenda.objects.filter(is_visible=True)
+
+
+class AgendaView(DetailView):
+    model = models.Agenda
+    template_name = 'agenda.html'
+
+    def get_object(self, queryset=None):
+        obj = super(AgendaView, self).get_object(queryset)
+        if not obj.is_visible:
+            raise Http404
+        else:
+            return obj
+
+
+class SubmitVotesView(DetailView):
+    model = models.Agenda
+    template_name = 'agenda.html'
+    http_method_names = [u'post']
+
+    def get_object(self, queryset=None):
+        obj = super(SubmitVotesView, self).get_object(queryset)
+        if not obj.is_visible:
+            raise Http404
+        else:
+            return obj
+
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            user = request.user
+        else:
+            return JsonResponse({'message': 'Fazer login'},
+                                status=403)
+
+        data = json.loads(request.POST.get('data'))
+        groups_count = len(data['groups'])
+        empty_groups = 0
+        for group in data['groups']:
+            votes = group['votes']
+            if len(votes) == 3:
+                self.create_votes(user, votes, group['groupId'])
+            elif len(votes) < 3 and len(votes) > 0:
+                return JsonResponse({'message': 'Gastar todos os votos'},
+                                    status=400)
+            else:
+                empty_groups += 1
+
+        if groups_count == empty_groups:
+            return JsonResponse({'message': 'Vote em pelo menos um'},
+                                status=400)
+        else:
+            return JsonResponse({'message': 'Tudo certo'})
+
+    def create_votes(self, user, votes, group_id):
+        for vote in votes:
+            models.Vote.objects.create(
+                user=user,
+                proposal_id=vote["name"],
+                proposal_group_id=group_id,
+                vote=self.get_vote_type(vote["value"]),
+                agenda=self.get_object()
+            )
+
+    def get_vote_type(self, vote):
+        if vote == 'upvote':
+            return True
+        else:
+            return False
