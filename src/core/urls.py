5913ac635a8b434a85a817f821717efd2418cd25
Caê Penna <caepenna@gmail.com>
2017-08-31 14:29:11 -0300

    ✨ Create new page describing the methodoogy (#76)
    * ✨💬 Create new page, url, raw markup and content
    * 🎨 Reorganize & refact project colors
    * 💄 Style method-page
    * 💬 Change text on homepage
    * 💬 Add text and method link to result page

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from core import views
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    url(r'^method/?$', TemplateView.as_view(template_name='pages/method.html'), name='method'),
     url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'^pauta/(?P<pk>\d+)/?$', views.AgendaView.as_view(),
         name='agenda'),
