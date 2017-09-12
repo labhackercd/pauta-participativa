62fed642ff6378fd0b65c88aeaa237b841b70441
Caê Penna <caepenna@gmail.com>
2017-09-12 13:58:40 -0300

    :bug: Add open graph tags to home page (#99)
    * Add open graph tags to home page
    
    * Reorganize metatags
    
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>
    
    * Add agenda metatags page
    
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- src/core/urls.py
+++ src/core/urls.py
@@ -3,8 +3,11 @@ from core import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^method/?$', TemplateView.as_view(template_name='pages/method.html'), name='method'),
+    url(r'^method/?$', TemplateView.as_view(template_name='pages/method.html'),
+        name='method'),
     url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'^pauta/(?P<pk>\d+)/?$', views.AgendaView.as_view(),
         name='agenda'),
+    url(r'^pauta/(?P<pk>\d+)/metas/?$', views.AgendaMetaView.as_view(),
+        name='agenda_meta'),
 ]
